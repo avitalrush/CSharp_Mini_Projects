d1d938d2035c806b20e08a61513d1298f66948c3
Avital Rush <avital29394@gmail.com>
2020-04-16 00:03:51 +0300

    My methods + Shaked's findType Method

--- B20_Ex01_4/Program.cs
+++ B20_Ex01_4/Program.cs
@@ -1,10 +1,127 @@
-﻿namespace B20_Ex01_4
+﻿using System;
+
+namespace B20_Ex01_4
 {
     public class Program
     {
-        static void Main()
-        {
+        private const int k_LenOfInput = 8;
+        private const int DIGIT = 0;
+        private const int LETTER = 1;
 
+        public static void Main()
+        {
+            string inputStr;
+
+            Console.WriteLine("Please enter 8-char string, with only digits or english letters: ");
+            inputStr = Console.ReadLine();
+            analyzeInputString(inputStr);
+
+            Console.ReadLine();
+        }
+
+        private static void analyzeInputString(string i_Str)
+        {
+            if(isPalindrome(i_Str))
+            {
+                Console.WriteLine("{0} is a Palindrome", i_Str);
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT a Palindrome", i_Str);
+            }
+
+            if (isDigitString(i_Str))
+            {
+                if (isDividedByFive(i_Str))
+                {
+                    Console.WriteLine("{0} is  divided by 5", i_Str);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is NOT divided by 5", i_Str);
+                }
+            }
+
+            if (isLetterString(i_Str))
+            {
+                int count = countUpperCaseLetters(i_Str);
+                Console.WriteLine("There are {0} uppercase letters in {1}", count, i_Str);
+            }
+        }
+
+        private static bool isPalindrome(string i_Str)
+        {
+                bool stringIsPalindrome;
+                int strLen = i_Str.Length;
+
+                if(strLen == 1 || strLen == 0)
+                {
+                    stringIsPalindrome = true;
+                }
+                else if (i_Str[0] != i_Str[strLen-1])
+                {
+                    stringIsPalindrome = false;
+                }
+                else
+                {
+                    stringIsPalindrome = isPalindrome(i_Str.Substring(1,strLen-2));
+                }
+
+                return stringIsPalindrome;
+        }
+
+        private static bool isDigitString(string i_Str)
+        {
+            return (findCharType(i_Str[0]) == DIGIT);
+
+            // it’s enough to check only the first char of the string
+            // because we already know it’s a valid string and all the other chars will be of the same type
+        }
+
+        private static int findCharType(char i_char)
+        {
+            int typeOfChar;
+
+            if(char.IsLetter(i_char)) // if the char is a English letter
+            {
+                typeOfChar = LETTER;
+            }
+            else
+            {
+                typeOfChar = DIGIT;   // because it can be either letter or digit, only 2 options
+            }
+
+            return typeOfChar;
+        }
+
+        private static bool isDividedByFive(string i_Str)
+        {
+            int numFromStr = int.Parse(i_Str);
+
+            return (numFromStr % 5 == 0);
+        }
+
+        private static bool isLetterString(string i_Str)
+        {
+            return (findCharType(i_Str[0]) == LETTER);
+
+            // it’s enough to check only the first char of the string
+            // because we already know it’s a valid string and all the other chars will be of the same type
+        }
+
+        private static int countUpperCaseLetters(string i_Str)
+        {
+            int upperCount = 0;
+
+            for(int i = 0; i <= k_LenOfInput-1; i++)
+            {
+                if(char.IsUpper(i_Str[i]))
+                {
+                    upperCount++;
+                }
+            }
+
+            return upperCount;
         }
     }
 }
